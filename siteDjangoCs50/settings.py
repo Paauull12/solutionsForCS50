4e1edffc42540186d1f618cfd793da9a19998ebb
Paauull12 <andrei.dobrescu@stud.ubbcluj.ro>
2024-07-14 14:02:38 +0300

    right

--- siteDjangoCs50/settings.py
+++ siteDjangoCs50/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-neki8n8qxhhnf!3%uoc!s=2a2axj+=ky&jho=53su84_z(1x3&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
